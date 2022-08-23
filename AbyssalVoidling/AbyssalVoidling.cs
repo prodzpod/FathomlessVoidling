e45e2ac6590094c8a38db4da605b425e78f9f42d
Romy Maghsoudi <romy.mag9@gmail.com>
2022-08-23 19:46:34 -0400

    v0.5.0

--- AbyssalVoidling/AbyssalVoidling.cs
+++ AbyssalVoidling/AbyssalVoidling.cs
@@ -14,48 +14,80 @@ using UnityEngine.AddressableAssets;
 
 namespace AbyssalVoidling
 {
-    [BepInPlugin("com.Nuxlar.AbyssalVoidling", "AbyssalVoidling", "1.0.0")]
+  [BepInPlugin("com.Nuxlar.AbyssalVoidling", "AbyssalVoidling", "0.5.0")]
 
-    public class AbyssalVoidling : BaseUnityPlugin
+  public class AbyssalVoidling : BaseUnityPlugin
+  {
+    GameObject voidRaidCrabPhase1 = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidRaidCrab/MiniVoidRaidCrabBodyPhase1.prefab").WaitForCompletion();
+    GameObject voidRaidCrabPhase2 = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidRaidCrab/MiniVoidRaidCrabBodyPhase2.prefab").WaitForCompletion();
+    GameObject voidRaidCrabPhase3 = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidRaidCrab/MiniVoidRaidCrabBodyPhase3.prefab").WaitForCompletion();
+
+    public void Awake()
     {
-        GameObject voidRaidCrabPhase1 = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidRaidCrab/MiniVoidRaidCrabBodyPhase1.prefab").WaitForCompletion();
-        GameObject voidRaidCrabPhase2 = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidRaidCrab/MiniVoidRaidCrabBodyPhase2.prefab").WaitForCompletion();
-        GameObject voidRaidCrabPhase3 = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/VoidRaidCrab/MiniVoidRaidCrabBodyPhase3.prefab").WaitForCompletion();
-
-        public void Awake()
-        {
-            On.RoR2.Run.Start += OnRunStart;
-        }
-
-        private void AdjustPhase1Stats()
-        {
-            CharacterBody voidRaidCrabBody = voidRaidCrabPhase1.GetComponent<CharacterBody>();
-
-            voidRaidCrabBody.baseMaxHealth = 1400;
-
-            voidRaidCrabBody.baseAttackSpeed = 1.25f;
-            voidRaidCrabBody.baseMoveSpeed = 100;
-            voidRaidCrabBody.baseAcceleration = 40;
-            ProjectileSteerTowardTarget voidRaidMissiles = new FireMissiles().projectilePrefab.GetComponent<ProjectileSteerTowardTarget>();
-            voidRaidMissiles.rotationSpeed = 180;
-
-            /** 
-                accel 20
-                armor 20
-                attkspd 1
-                dmg 15
-                HP 2000
-                movespd 45
-                jump 0
-                projectile rotation 180
-            **/
-        }
-
-        private void OnRunStart(On.RoR2.Run.orig_Start orig, Run self)
-        {
-            AdjustPhase1Stats();
-            orig(self);
-        }
-
+      On.RoR2.Run.Start += Run_Start;
+      On.EntityStates.VoidRaidCrab.BaseSpinBeamAttackState.OnEnter += BaseSpinBeamAttackState_OnEnter;
     }
+    /** 
+        Base Stats
+        accel 20
+        armor 20
+        attkspd 1
+        dmg 15
+        HP 2000
+        movespd 45
+        jump 0
+        projectile rotation 360
+    **/
+    private void AdjustPhase1Stats()
+    {
+      CharacterBody voidRaidCrabBody = voidRaidCrabPhase1.GetComponent<CharacterBody>();
+      voidRaidCrabBody.baseMaxHealth = 1250;
+      voidRaidCrabBody.baseAttackSpeed = 1.5f;
+      voidRaidCrabBody.baseMoveSpeed = 65;
+      voidRaidCrabBody.baseAcceleration = 40;
+      ProjectileSteerTowardTarget voidRaidMissiles = new FireMissiles().projectilePrefab.GetComponent<ProjectileSteerTowardTarget>();
+      voidRaidMissiles.rotationSpeed = 160;
+    }
+    private void AdjustPhase2Stats()
+    {
+      CharacterBody voidRaidCrabBody = voidRaidCrabPhase2.GetComponent<CharacterBody>();
+      voidRaidCrabBody.baseMaxHealth = 1500;
+      voidRaidCrabBody.baseAttackSpeed = 1.75f;
+      voidRaidCrabBody.baseMoveSpeed = 85;
+      voidRaidCrabBody.baseAcceleration = 45;
+      ProjectileSteerTowardTarget voidRaidMissiles = new FireMissiles().projectilePrefab.GetComponent<ProjectileSteerTowardTarget>();
+      voidRaidMissiles.rotationSpeed = 180;
+    }
+    private void AdjustPhase3Stats()
+    {
+      CharacterBody voidRaidCrabBody = voidRaidCrabPhase3.GetComponent<CharacterBody>();
+      voidRaidCrabBody.baseMaxHealth = 1750;
+      voidRaidCrabBody.baseAttackSpeed = 2;
+      voidRaidCrabBody.baseMoveSpeed = 105;
+      voidRaidCrabBody.baseAcceleration = 50;
+      ProjectileSteerTowardTarget voidRaidMissiles = new FireMissiles().projectilePrefab.GetComponent<ProjectileSteerTowardTarget>();
+      voidRaidMissiles.rotationSpeed = 200;
+    }
+
+    private void Run_Start(On.RoR2.Run.orig_Start orig, Run self)
+    {
+      AdjustPhase1Stats();
+      AdjustPhase2Stats();
+      AdjustPhase3Stats();
+      orig(self);
+    }
+
+    private void BaseSpinBeamAttackState_OnEnter(On.EntityStates.VoidRaidCrab.BaseSpinBeamAttackState.orig_OnEnter orig, EntityStates.VoidRaidCrab.BaseSpinBeamAttackState self)
+    {
+      self.baseDuration = 3.5f;
+      orig(self);
+    }
+
+    private void BaseVacuumAttackState_OnEnter(On.EntityStates.VoidRaidCrab.BaseVacuumAttackState.orig_OnEnter orig, EntityStates.VoidRaidCrab.BaseVacuumAttackState self)
+    {
+      self.baseDuration = 5.5f;
+      orig(self);
+    }
+
+  }
 }
--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Abyssal Voidling
 
-Lorem Ipsum.
+Aiming to make this an "Accursed" version of Voidling. Right now HP and missile tracking is lowered to be more reasonable, move speed and attack speed are increased, spinbeam and vacuum attacks are faster as well. Each of these increase with each phase so there's some difficulty progression and it doesn't feel as lame. 
 
 Phase info:
 
 - Phase 1 Lorem Ipsum.
-- Phase 2 Lorem Ipsum.
-- Phase 3 Lorem Ipsum.
+- Phase 2 Lorem Ipsum Electric Bugaloo.
+- Phase 3 Lorem Ipsum the squeakquel.
+
+## Plans
+
+- Full configurability
+- Adding a gimmick/ability for each phase so there's a larger difference between phases
+- Adding a deterrent for just sitting on top the mf
+- Adding some stuff to make it more fun
 
 ## Changelog
-**1.0.0**
+**0.5.0**
 
-- Release!
+- Decreases HP and Missile tracking
+- All stats increase with each phase
+- Faster spinbeam and vacuum
